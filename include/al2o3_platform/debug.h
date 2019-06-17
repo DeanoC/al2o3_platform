1bad1e43ed99ab377709b3cf6fd1dbf08d7f50f7
DeanoC <deano@cloudpixies.com>
2019-06-17 23:55:58 +0300

    Update debug.h
    tidied up

--- include/al2o3_platform/debug.h
+++ include/al2o3_platform/debug.h
@@ -8,8 +8,9 @@
 #define USE_LOGGING 1
 #endif // end USE_LOGGING
 
+// These functions are actually called, allows for transparent overriding of the log functions
+// defaults to printf/outputdebugstring
 typedef struct AL2O3_Logger_t {
-	// lowest level of logging, (file, line, message)
 	void (*errorMsg)(char const* file, int line, const char* function, char const* msg);
 	void (*warningMsg)(char const* file, int line, const char* function, char const* msg);
 	void (*infoMsg)(char const* file, int line, const char* function, char const* msg);
@@ -17,60 +18,59 @@ typedef struct AL2O3_Logger_t {
 
 	void (*failedAssert)(char const* file, int line, const char* function, char const* statement);
 } AL2O3_Logger_t;
-
 AL2O3_EXTERN_C AL2O3_Logger_t AL2O3_Logger;
 
+// lowest level debug output
 AL2O3_EXTERN_C void AL2O3_OutputDebug(char const *msg);
 
+// call the functions in the global logger function 
 AL2O3_EXTERN_C void AL2O3_ErrorMsg(const char* file, int line, const char* function, const char* string, ...);
 AL2O3_EXTERN_C void AL2O3_WarningMsg(const char* file, int line, const char* function, const char* string, ...);
 AL2O3_EXTERN_C void AL2O3_InfoMsg(const char* file, int line, const char* function, const char* string, ...);
 AL2O3_EXTERN_C void AL2O3_DebugMsg(const char* file, int line, const char* function, const char* string, ...);
-
 AL2O3_EXTERN_C void AL2O3_FailedAssert(const char* file, int line, const char* function, const char* statement);
 
-#define ErrorMsg(str, ...) AL2O3_ErrorMsg(__LINE__, __FILE__, __FUNCTION__, str, ##__VA_ARGS__)
-#define WarningMsg(str, ...) AL2O3_WarningMsg(__LINE__, __FILE__, __FUNCTION__, str, ##__VA_ARGS__)
-#define InfoMsg(str, ...) AL2O3_InfoMsg(__LINE__, __FILE__, __FUNCTION__, str, ##__VA_ARGS__)
-
 #ifndef NDEBUG
-
-#define DebugMsg(str, ...) AL2O3_DebugMsg(__LINE__, __FILE__, __FUNCTION__, str, ##__VA_ARGS__)
-#define IFASSERT(x) x
 #if defined(ORBIS)
 // there is a large amount of stuff included via header files ...
 #define ASSERT(cond) SCE_GNM_ASSERT(cond)
 #else //!ORBIS
 #define ASSERT(b) { if(!(b)) { AL2O3_FailedAssert(__FILE__, __LINE__, __FUNCTION__, #b); } }
 #endif //end !ORBIS
-
 #else //!NDEBUG
-#define DebugMsg(str, ...)
 #define ASSERT(b)
-#define IFASSERT(x)
 #endif //end !DEBUG
 
 #ifdef USE_LOGGING
+#ifndef NDEBUG
 #define LOGDEBUG(message) AL2O3_DebugMsg( __FILE__, __LINE__, __FUNCTION__, message)
-#define LOGINFO(message) AL2O3_InfoMsg( __FILE__, __LINE__, __FUNCTION__, message)
-#define LOGWARNING(message) AL2O3_WarningMsg( __FILE__, __LINE__, __FUNCTION__, message)
-#define LOGERROR(message) AL2O3_ErrorMsg( __FILE__, __LINE__, __FUNCTION__, message)
-
 #define LOGDEBUGF(message, ...) AL2O3_DebugMsg( __FILE__, __LINE__, __FUNCTION__, message, ##__VA_ARGS__)
+#else
+#define LOGDEBUG(message) ((void)0)
+#define LOGDEBUGF(message, ...) ((void)0)
+#endif // end NDEBUG
+
+#define LOGINFO(message) AL2O3_InfoMsg( __FILE__, __LINE__, __FUNCTION__, message)
 #define LOGINFOF(message, ...) AL2O3_InfoMsg( __FILE__, __LINE__, __FUNCTION__, message, ##__VA_ARGS__)
+
+#define LOGWARNING(message) AL2O3_WarningMsg( __FILE__, __LINE__, __FUNCTION__, message)
 #define LOGWARNINGF(message, ...) AL2O3_WarningMsg( __FILE__, __LINE__, __FUNCTION__, message, ##__VA_ARGS__)
+
+#define LOGERROR(message) AL2O3_ErrorMsg( __FILE__, __LINE__, __FUNCTION__, message)
 #define LOGERRORF(message, ...) AL2O3_ErrorMsg( __FILE__, __LINE__, __FUNCTION__, message, ##__VA_ARGS__)
 
 #else //!USE_LOGGING
+
 #define LOGDEBUG(message) ((void)0)
 #define LOGINFO(message) ((void)0)
 #define LOGWARNING(message) ((void)0)
 #define LOGERROR(message) ((void)0)
 
-#define LOGDEBUGF(...) ((void)0)
-#define LOGINFOF(...) ((void)0)
-#define LOGWARNINGF(...) ((void)0)
-#define LOGERRORF(...) ((void)0)
+#define LOGDEBUGF(message, ...) ((void)0)
+#define LOGINFOF(message, ...) ((void)0)
+#define LOGWARNINGF(message, ...) ((void)0)
+#define LOGERRORF(message, ...) ((void)0)
+
 #endif //end !USE_LOGGING
 
 #endif //AL2O3_PLATFORM_DEBUG_H
