eb8cdac04659a9a9d92c94e59d9b02b5fb233bd7
Deano <me@deanoc.com>
2019-09-16 22:28:17 +0300

    visual debug WIP

--- include/al2o3_platform/visualdebug.h
+++ include/al2o3_platform/visualdebug.h
@@ -16,13 +16,16 @@
 // defaults to logging
 typedef struct AL2O3_VisualDebugging_t {
 
-	void (*Line)(float p0[3], float p1[3], float colour[4]);
+	void (*Line)(float p0x, float p0y, float p0z, float p1x, float p1y, float p1z, uint32_t colour);
 
 } AL2O3_VisualDebugging_t;
 AL2O3_EXTERN_C AL2O3_VisualDebugging_t AL2O3_VisualDebugging;
 
+#define VISDEBUG_PACKCOLOUR(r, g, b, a) ((r) << 24) | ((g) << 16) | ((b) << 8) | ((a) << 0)
+
 #ifdef USE_VISUAL_DEBUGGING
-#define VISDEBUG_LINE(p0, p1, colour) AL2O3_VisualDebugging->Line(p0, p1, colour)
+#define VISDEBUG_LINE(p0x, p0y, p0z, p1x, p1y, p1z, packedcolor) \
+  AL2O3_VisualDebugging.Line(p0x, p0y, p0z,  p1x, p1y, p1z, packedcolor)
 #else
 #define VISDEBUG_LINE(p0, p1, colour) ((void)0)
 #endif
--- src/visualdebug.c
+++ src/visualdebug.c
@@ -2,13 +2,14 @@
 #include "al2o3_platform/debug.h"
 #include "al2o3_platform/visualdebug.h"
 
-static void LineLog(float p0[3], float p1[3], float colour[4]) {
+static void LineLog(float p0x, float p0y, float p0z, float p1x, float p1y, float p1z, uint32_t colour) {
+
 	LOGINFO("Line from <%1.2f, %1.2f, %1.2f> to "
 					"<%1.2f, %1.2f, %1.2f> of "
-					"<%1.2f, %1.2f, %1.2f,%1.2>",
-					p0[0], p0[1], p0[2],
-					p1[0], p1[1], p1[2],
-					colour[0], colour[1], colour[2], colour[3]);
+					"0x%x",
+					p0x, p0y, p0z,
+					p1x, p1y, p1z,
+					colour);
 }
 
 AL2O3_EXTERN_C AL2O3_VisualDebugging_t AL2O3_VisualDebugging = {
