9a59d888cc0168b50df0c8766d3e99c594669e4d
Deano <me@deanoc.com>
2019-10-07 18:09:51 +0300

    Added helper function to fixed descriptor set but with frequently updated data (not bindings) which is a common case. Fixxed Render_Basic entire viewport and scissor bug debugGpuview now working MemoryTrackering  now can set allocation break from anywhere Visual debugger working and quads and tris added Visual debugger pick colour per primitive added  Math Look At Matrix function added  Math vector length and normalise added for real vectors  Math Cross Product added for Vec3F  Fixed visual debug shader  Fixed VISDEBUG_PACKCOLOUR

--- include/al2o3_platform/visualdebug.h
+++ include/al2o3_platform/visualdebug.h
@@ -6,6 +6,9 @@
 /// Having a very low level interface allows no coupling between being able to
 /// display basic visual debugging information and any actual renderer.
 
+// the single colour solid primitives with a colour of 0 will generate a per primitive
+// colour from a internal table, that should make each face visible
+
 #include "al2o3_platform/platform.h"
 
 #ifndef USE_VISUAL_DEBUGGING
@@ -17,15 +20,40 @@
 typedef struct AL2O3_VisualDebugging_t {
 
 	void (*Line)(float p0x, float p0y, float p0z, float p1x, float p1y, float p1z, uint32_t colour);
+	void (*Lines)(uint32_t lineCount, float const * verts, uint32_t colour);
+	void (*LineStrip)(uint32_t lineCount, float const * verts, uint32_t colour);
+	void (*SolidTris)(uint32_t triCount, float const * verts, uint32_t colour);
+	void (*SolidQuads)(uint32_t quadCount, float const * verts, uint32_t colour);
 
 } AL2O3_VisualDebugging_t;
 AL2O3_EXTERN_C AL2O3_VisualDebugging_t AL2O3_VisualDebugging;
 
-#define VISDEBUG_PACKCOLOUR(r, g, b, a) ((r) << 24) | ((g) << 16) | ((b) << 8) | ((a) << 0)
+#define VISDEBUG_PACKCOLOUR(r, g, b, a) (((uint32_t)r) << 0) | ((g) << 8) | ((b) << 16) | ((a) << 24)
 
 #ifdef USE_VISUAL_DEBUGGING
+
 #define VISDEBUG_LINE(p0x, p0y, p0z, p1x, p1y, p1z, packedcolor) \
   AL2O3_VisualDebugging.Line(p0x, p0y, p0z,  p1x, p1y, p1z, packedcolor)
+#define VISDEBUG_SOLID_TRI(p0x, p0y, p0z, p1x, p1y, p1z, p2x, p2y, p2z, packedcolor) \
+	AL2O3_VisualDebugging.SolidTri(p0x, p0y, p0z,  p1x, p1y, p1z, p2x, p2y, p2z, packedcolor)
+
+#define VISDEBUG_LINES(lineCount, verts, packedcolor) \
+  AL2O3_VisualDebugging.Lines(lineCount, verts, packedcolor)
+#define VISDEBUG_LINESTRIP(lineCount, verts, packedcolor) \
+  AL2O3_VisualDebugging.LineStrip(lineCount, verts, packedcolor)
+
+#define VISDEBUG_SOLID_TRIS(triCount, verts, packedcolor) \
+  AL2O3_VisualDebugging.SolidTris(triCount, verts, packedcolor)
+#define VISDEBUG_SOLID_QUADS(quadCount, verts, packedcolor) \
+  AL2O3_VisualDebugging.SolidQuads(quadCount, verts, packedcolor)
+
 #else
-#define VISDEBUG_LINE(p0, p1, colour) ((void)0)
+
+#define VISDEBUG_LINE(p0x, p0y, p0z, p1x, p1y, p1z, packedcolor) ((void)0)
+#define VISDEBUG_SOLID_TRI(p0x, p0y, p0z, p1x, p1y, p1z, p2x, p2y, p2z, packedcolor) ((void)0)
+
+#define VISDEBUG_LINES(lineCount, verts, packedcolor) ((void)0)
+#define VISDEBUG_SOLID_TRIS(triCount, verts, packedcolor) ((void)0)
+#define VISDEBUG_SOLID_QUADS(quadCount, verts, packedcolor)  ((void)0)
+
 #endif
