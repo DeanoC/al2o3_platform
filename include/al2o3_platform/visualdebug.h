ee3b4f9f32244f33a8168f6edea2efdab218d750
Deano <me@deanoc.com>
2019-10-08 18:04:17 +0300

    WIP instances debug platonicsolids

--- include/al2o3_platform/visualdebug.h
+++ include/al2o3_platform/visualdebug.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /// Visual debug are primitives that can be rendered onto a screen
-/// By default that just log the parameters, but if the application has a
+/// By default they do nothing, but if the application has a
 /// renderer, they are expected to be hooked and rendered.
 /// Having a very low level interface allows no coupling between being able to
 /// display basic visual debugging information and any actual renderer.
@@ -21,9 +21,14 @@ typedef struct AL2O3_VisualDebugging_t {
 
 	void (*Line)(float p0x, float p0y, float p0z, float p1x, float p1y, float p1z, uint32_t colour);
 	void (*Lines)(uint32_t lineCount, float const * verts, uint32_t colour);
+
 	void (*LineStrip)(uint32_t lineCount, float const * verts, uint32_t colour);
 	void (*SolidTris)(uint32_t triCount, float const * verts, uint32_t colour);
 	void (*SolidQuads)(uint32_t quadCount, float const * verts, uint32_t colour);
+
+	// euler rotation are in radians
+	void (*Tetrahedron)(float const* pos, float const* eulerRots, float const* scale, uint32_t colour);
+	void (*Cube)(float const* pos, float const* eulerRots, float const* scale, uint32_t colour);
 
 } AL2O3_VisualDebugging_t;
 AL2O3_EXTERN_C AL2O3_VisualDebugging_t AL2O3_VisualDebugging;
@@ -47,6 +52,12 @@ AL2O3_EXTERN_C AL2O3_VisualDebugging_t AL2O3_VisualDebugging;
 #define VISDEBUG_SOLID_QUADS(quadCount, verts, packedcolor) \
   AL2O3_VisualDebugging.SolidQuads(quadCount, verts, packedcolor)
 
+#define VISDEBUG_TETRAHEDRON(pos, rot, scale, packedcolor) \
+  AL2O3_VisualDebugging.Tetrahedron(pos, rot, scale, packedcolor)
+
+#define VISDEBUG_CUBE(pos, rot, scale, packedcolor) \
+  AL2O3_VisualDebugging.Cube(pos, rot, scale, packedcolor)
+
 #else
 
 #define VISDEBUG_LINE(p0x, p0y, p0z, p1x, p1y, p1z, packedcolor) ((void)0)
@@ -54,6 +65,9 @@ AL2O3_EXTERN_C AL2O3_VisualDebugging_t AL2O3_VisualDebugging;
 
 #define VISDEBUG_LINES(lineCount, verts, packedcolor) ((void)0)
 #define VISDEBUG_SOLID_TRIS(triCount, verts, packedcolor) ((void)0)
-#define VISDEBUG_SOLID_QUADS(quadCount, verts, packedcolor)  ((void)0)
+#define VISDEBUG_SOLID_QUADS(quadCount, verts, packedcolor) ((void)0)
+
+#define VISDEBUG_TETRAHEDRON(pos, rot, scale, packedcolor) ((void)0)
+#define VISDEBUG_CUBE(pos, rot, scale, packedcolor) ((void)0)
 
 #endif
--- src/visualdebug.c
+++ src/visualdebug.c
@@ -2,18 +2,23 @@
 #include "al2o3_platform/debug.h"
 #include "al2o3_platform/visualdebug.h"
 
-static void LineLog(float p0x, float p0y, float p0z, float p1x, float p1y, float p1z, uint32_t colour) {
+static void Line(float p0x, float p0y, float p0z, float p1x, float p1y, float p1z, uint32_t colour) {}
+static void Lines(uint32_t lineCount, float const * verts, uint32_t colour) {}
+static void LineStrip(uint32_t lineCount, float const * verts, uint32_t colour) {}
+static void SolidTris(uint32_t triCount, float const * verts, uint32_t colour) {}
+static void SolidQuads(uint32_t quadCount, float const * verts, uint32_t colour) {}
+static void Tetrahedron(float const* pos, float const* eulerRots, float const* scale, uint32_t colour) {}
+static void Cube(float const* pos, float const* eulerRots, float const* scale, uint32_t colour) {}
 
-	LOGINFO("Line from <%1.2f, %1.2f, %1.2f> to "
-					"<%1.2f, %1.2f, %1.2f> of "
-					"0x%x",
-					p0x, p0y, p0z,
-					p1x, p1y, p1z,
-					colour);
-}
 
 AL2O3_EXTERN_C AL2O3_VisualDebugging_t AL2O3_VisualDebugging = {
-		&LineLog,
+		&Line,
+		&Lines,
+		&LineStrip,
+		&SolidTris,
+		&SolidQuads,
+		&Tetrahedron,
+		&Cube,
 };
 
 
